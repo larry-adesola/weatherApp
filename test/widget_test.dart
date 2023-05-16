eecd27715d1503bef3071d7d5f3519b9ef7a7469
khana <khananeesah@ymail.com>
2023-05-16 14:55:30 +0100

    I added a general scale to settings

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,6 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_app/main.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());

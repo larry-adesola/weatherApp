f9acb068909a0bbba59285ce693fa3a5c8589382
Matteo <ma2046@cam.ac.uk>
2023-05-21 20:13:04 +0100

    it works as long as i dont merge

--- lib/pages/suggestionScreen.dart
+++ lib/pages/suggestionScreen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:weather_app/data.dart';
+import 'package:weather_app/pages/detailedDialog.dart';
+import 'package:weather_app/users.dart';
+import 'package:weather_app/weatherapi.dart';
+import 'package:converter/converter.dart';
+import 'package:lottie/lottie.dart';
 
 class SuggScreen extends StatefulWidget {
   const SuggScreen({Key? key}) : super(key: key);
-
 
   @override
   State<SuggScreen> createState() => _SuggScreenState();
@@ -14,53 +19,351 @@ class _SuggScreenState extends State<SuggScreen> {
   final String currentDay = DateFormat('EEE').format(DateTime.now());
   final days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 
+  String _twoDigits(int n) {
+    if (n >= 10) return '$n';
+    return '0$n';
+  }
+
   @override
   Widget build(BuildContext context) {
-    final size = MediaQuery
-        .of(context)
-        .size;
+    final size = MediaQuery.of(context).size;
     final currentDayIndex = days.indexOf(currentDay);
 
-    return (
-        Positioned(
-            top: 25,
-            left: 25,
-            child: ButtonBar(
-                children: [
-                  OutlinedButton(
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 50), // Move buttons down by 50 pixels
+                child: ButtonBar(
+                  children: [
+                    ElevatedButton(
                       onPressed: () {
-                        _selectedDay = 0;
+                        setState(() {
+                          _selectedDay = 0;
+                        });
                       },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(70, 70), backgroundColor: _selectedDay == 0 ? Colors.blue : null,
+                        shape: const CircleBorder(), // Change color when selected
+                      ),
                       child: Text(
-                          currentDay,
-                          style: TextStyle(color: Colors.black)
-                      )
-                  ),
-                  OutlinedButton(
+                        currentDay,
+                        style: TextStyle(
+                          color: _selectedDay == 0 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
                       onPressed: () {
-                        _selectedDay = 1;
+                        setState(() {
+                          _selectedDay = 1;
+                        });
                       },
-                      child: Text(days[(currentDayIndex + 1) % 7],
-                          style: TextStyle(color: Colors.black))
-                  ),
-                  OutlinedButton(
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(70, 70),
+                        shape: const CircleBorder(),
+                        backgroundColor: _selectedDay == 1 ? Colors.blue : null, // Change color when selected
+                      ),
+                      child: Text(
+                        days[(currentDayIndex + 1) % 7],
+                        style: TextStyle(
+                          color: _selectedDay == 1 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
                       onPressed: () {
-                        _selectedDay = 2;
+                        setState(() {
+                          _selectedDay = 2;
+                        });
                       },
-                      child: Text(days[(currentDayIndex + 2) % 7],
-                          style: const TextStyle(color: Colors.black))
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(70, 70), backgroundColor: _selectedDay == 2 ? Colors.blue : null,
+                        shape: const CircleBorder(), // Change color when selected
+                      ),
+                      child: Text(
+                        days[(currentDayIndex + 2) % 7],
+                        style: TextStyle(
+                          color: _selectedDay == 2 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _selectedDay = 3;
+                        });
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(70, 70), backgroundColor: _selectedDay == 3 ? Colors.blue : null,
+                        shape: const CircleBorder(), // Change color when selected
+                      ),
+                      child: Text(
+                        days[(currentDayIndex + 3) % 7],
+                        style: TextStyle(
+                          color: _selectedDay == 3 ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          FutureBuilder(
+              future: Weather().getForecastSuggestion(_selectedDay,4,11),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Column(
+                    children: [
+                      Lottie.asset('assets/anims/loading.json'),
+                      const Text('Loading')
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return const Text('Error creating suggestions',
+                    style: TextStyle(fontSize: 10),);
+                }
+                print(_selectedDay);
+                print(snapshot.data?['dt']);
+                String time = DateFormat('HH:mm').format(DateTime.fromMillisecondsSinceEpoch(snapshot.data?['dt'] * 1000).toLocal()).substring(0, 5);
+                String hourPlusOne = '${_twoDigits(int.parse(time.substring(0, 2)) + 1)}:00';
+                return
+                  Container(
+                      width: size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.blue[700]!, width: 2.5),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Column(children: [
+                        SizedBox(
+                          height: size.height * 0.025,
+                        ),
+                        Text(
+                          '$time - $hourPlusOne',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.025,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                SizedBox(
+                                    width: size.width * 0.2,
+                                    child: Image.asset(
+                                        WeatherData().weatherIcons[
+                                        snapshot
+                                            .data?['weather'][0]['description']]!)),
+                                SizedBox(
+                                  height: size.height * 0.005,
+                                ),
+                                Text(snapshot
+                                    .data?['weather'][0]['description']!)
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.thermostat, size: 40,),
+                                    Text(
+                                      '${Temperature(
+                                          snapshot.data!['main']['temp'], 'K')
+                                          .valueIn('C')
+                                          .round()} °C',
+                                      style: const TextStyle(fontSize: 30),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
 
-                  ),
-                  OutlinedButton(
-                      onPressed: () {
-                        _selectedDay = 3;
-                      },
-                      child: Text(days[(currentDayIndex + 3) % 7],
-                          style: const TextStyle(color: Colors.black)))
-                ]
-            )
-        )
+                          ],
+                        ),
+                        SizedBox(
+                          height: size.height * 0.02,
+                        )
+                      ])
+                  );
+              }),
+          FutureBuilder(
+              future: Weather().getForecastSuggestion(_selectedDay,12,16),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Column(
+                    children: [
+                      Lottie.asset('assets/anims/loading.json'),
+                      const Text('Loading')
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return const Text('Error creating suggestions',
+                    style: TextStyle(fontSize: 10),);
+                }
+                print(_selectedDay);
+                print(snapshot.data?['dt']);
+                String time = DateFormat('HH:mm').format(DateTime.fromMillisecondsSinceEpoch(snapshot.data?['dt'] * 1000).toLocal()).substring(0, 5);
+                String hourPlusOne = '${_twoDigits(int.parse(time.substring(0, 2)) + 1)}:00';
+                return
+                  Container(
+                      width: size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.blue[700]!, width: 2.5),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Column(children: [
+                        SizedBox(
+                          height: size.height * 0.025,
+                        ),
+                        Text(
+                          '$time - $hourPlusOne',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.025,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                SizedBox(
+                                    width: size.width * 0.2,
+                                    child: Image.asset(
+                                        WeatherData().weatherIcons[
+                                        snapshot
+                                            .data?['weather'][0]['description']]!)),
+                                SizedBox(
+                                  height: size.height * 0.005,
+                                ),
+                                Text(snapshot
+                                    .data?['weather'][0]['description']!)
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.thermostat, size: 40,),
+                                    Text(
+                                      '${Temperature(
+                                          snapshot.data!['main']['temp'], 'K')
+                                          .valueIn('C')
+                                          .round()} °C',
+                                      style: const TextStyle(fontSize: 30),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                        SizedBox(
+                          height: size.height * 0.02,
+                        )
+                      ])
+                  );
+              }),
+          FutureBuilder(
+              future: Weather().getForecastSuggestion(_selectedDay,17,22),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Column(
+                    children: [
+                      Lottie.asset('assets/anims/loading.json'),
+                      const Text('Loading')
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return const Text('Error creating suggestions',
+                    style: TextStyle(fontSize: 10),);
+                }
+                String time = DateFormat('HH:mm').format(DateTime.fromMillisecondsSinceEpoch(snapshot.data?['dt'] * 1000).toLocal()).substring(0, 5);
+                String hourPlusOne = '${_twoDigits(int.parse(time.substring(0, 2)) + 1)}:00';
+                return
+                  Container(
+                      width: size.width * 0.9,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Colors.blue[700]!, width: 2.5),
+                          borderRadius: BorderRadius.circular(30)),
+                      child: Column(children: [
+                        SizedBox(
+                          height: size.height * 0.025,
+                        ),
+                        Text(
+                          '$time - $hourPlusOne',
+                          style: const TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.025,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Column(
+                              children: [
+                                SizedBox(
+                                    width: size.width * 0.2,
+                                    child: Image.asset(
+                                        WeatherData().weatherIcons[
+                                        snapshot
+                                            .data?['weather'][0]['description']]!)),
+                                SizedBox(
+                                  height: size.height * 0.005,
+                                ),
+                                Text(snapshot
+                                    .data?['weather'][0]['description']!)
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                                Row(
+                                  children: [
+                                    const Icon(Icons.thermostat, size: 40,),
+                                    Text(
+                                      '${Temperature(
+                                          snapshot.data!['main']['temp'], 'K')
+                                          .valueIn('C')
+                                          .round()} °C',
+                                      style: const TextStyle(fontSize: 30),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+
+                          ],
+                        ),
+                        SizedBox(
+                          height: size.height * 0.02,
+                        )
+                      ])
+                  );
+              })
+        ],
+      ),
     );
   }
 }
-  
--- lib/weatherapi.dart
+++ lib/weatherapi.dart
@@ -56,25 +56,63 @@ class Weather {
       );
 
       // Find the closest matching forecast for the target time
-      final closestForecast = forecastList.reduce((a, b) {
-        final aTime = DateTime.fromMillisecondsSinceEpoch(a['dt'] * 1000)
-            .toLocal();
-        final bTime = DateTime.fromMillisecondsSinceEpoch(b['dt'] * 1000)
-            .toLocal();
-        final aDiff = aTime
-            .difference(targetDateTime)
-            .inSeconds
-            .abs();
-        final bDiff = bTime
-            .difference(targetDateTime)
-            .inSeconds
-            .abs();
-        return aDiff < bDiff ? a : b;
-      });
+      var closestForecast = GetClosestForecast(forecastList, targetDateTime);
       // Extract the desired weather information from the closest forecast
       return closestForecast;
     } else {
       throw Exception('Failed to fetch weather data');
     }
+  }
+  Future<Map<String, dynamic>> getForecastSuggestion(int selectedDay,int earliestHour,int latestHour) async {
+    //This method will take the selected day as an int offset from today (e.g. 1 is tomorrow) and return a forecast
+    //for a suggested time on that day
+    final apiKey = UserInfo().getAPIkey();
+    final encodedCityName = Uri.encodeComponent(UserInfo().getCity());
+    final url = 'https://api.openweathermap.org/data/2.5/forecast?q=$encodedCityName&appid=$apiKey';
+    final response = await http.get(Uri.parse(url));
+    final decodedResponse = jsonDecode(response.body);
+    const int secondsInDay = 24* 60 * 60;
+    if (response.statusCode == 200) {
+      final forecastList = decodedResponse['list'];
+      print(forecastList);
+      int secondsSinceEpoch = DateTime
+          .now()
+          .millisecondsSinceEpoch ~/ 1000;
+      int startOfSelectedDay;
+      int endOfSelectedDay;
+      if (selectedDay == 0) {
+        startOfSelectedDay = secondsSinceEpoch;
+        endOfSelectedDay = startOfSelectedDay + secondsInDay;
+      }else {
+        startOfSelectedDay =
+            secondsSinceEpoch - secondsSinceEpoch % secondsInDay +
+                selectedDay * secondsInDay;
+        endOfSelectedDay = (startOfSelectedDay + secondsInDay);
+      }
+      var targetDateTime = DateTime.fromMillisecondsSinceEpoch(startOfSelectedDay * 1000 + earliestHour * 3600000);
+      var closestForecast = GetClosestForecast(forecastList, targetDateTime);
+      return closestForecast;
+    } else {
+      throw Exception('Failed to fetch weather data');
+    }
+  }
+
+  GetClosestForecast(forecastList, DateTime targetDateTime) {
+    final closestForecast = forecastList.reduce((a, b) {
+      final aTime = DateTime.fromMillisecondsSinceEpoch(a['dt'] * 1000)
+          .toLocal();
+      final bTime = DateTime.fromMillisecondsSinceEpoch(b['dt'] * 1000)
+          .toLocal();
+      final aDiff = aTime
+          .difference(targetDateTime)
+          .inSeconds
+          .abs();
+      final bDiff = bTime
+          .difference(targetDateTime)
+          .inSeconds
+          .abs();
+      return aDiff < bDiff ? a : b;
+    });
+    return closestForecast;
   }
 }

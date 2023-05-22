6d0a804a5ee87890553f96284044c2c1b2e383fc
Emmanuel Adesola <adesola.emmanuel@yahoo.com>
2023-05-22 13:04:21 +0100

    stop device orientation make ui move

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 import 'package:weather_app/pages/homeBase.dart';
 import 'package:weather_app/pages/welcomeBase.dart';
@@ -15,6 +16,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);//stops device rotating
     return MaterialApp(
       title: 'Weather App',
       theme: ThemeData(
